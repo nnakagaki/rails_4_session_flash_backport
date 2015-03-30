2a90368aeed0798242c78f058c1460cc24e96578
Norris Nakagaki <norrisnakagaki@gmail.com>
2015-03-30 17:13:13 -0400

    testing

--- lib/rails_4_session_flash_backport/rails4/flash_hash.rb
+++ lib/rails_4_session_flash_backport/rails4/flash_hash.rb
@@ -6,44 +6,22 @@ require 'action_dispatch/middleware/flash'
 module ActionDispatch
   class Flash
     class FlashHash
-      def self.from_session_value(value)
-        flashes = discard = nil
+      def self.from_session_value(value) #:nodoc:
+        flash = case value
+                when FlashHash # Rails 3.1, 3.2
+                  new(value.instance_variable_get(:@flashes), value.instance_variable_get(:@used))
+                when Hash # Rails 4.0
+                  new(value['flashes'], value['discard'])
+                else
+                  new
+                end
 
-        case value
-        when ::ActionController::Flash::FlashHash # Rails 2.x
-          flashes = Hash.new.update(value)
-          discard = value.instance_variable_get(:@used).select{|a,b| b}.keys
-        when ::ActionDispatch::Flash::FlashHash # Rails 3.1, 3.2
-          flashes = value.instance_variable_get(:@flashes)
-          discard = value.instance_variable_get(:@used)
-        when Hash # Rails 4.0, we backported to 2.3 too
-          flashes = value['flashes']
-          discard = value['discard']
-        end
-
-        flashes ||= {}
-        if discard
-          flashes.except!(*discard)
-        end
-
-        new(flashes, flashes.keys)
+        flash.tap(&:sweep)
       end
 
-      def to_session_value
-        flashes_to_keep = @flashes.except(*@discard)
-        return nil if flashes_to_keep.empty?
-        {'flashes' => flashes_to_keep}
-      end
-    end
-  end
-end
-
-# This magic here allows us to unmarshal the old Rails 2.x ActionController::Flash::FlashHash
-module ActionController
-  module Flash
-    class FlashHash < Hash
-      def self._load(args)
-        {}
+      def to_session_value #:nodoc:
+        return nil if empty?
+        {'discard' => @discard.to_a, 'flashes' => @flashes}
       end
     end
   end
